e9c4d2929b5d74f2d0b032bef897a49946cb2df9
Szymka <1210022074@qq.com>
2020-05-15 14:36:53 +0800

    第二个主页面wxml逻辑错误已修改

--- frontend/Vphere/miniprogram/pages/absence/absence.js
+++ frontend/Vphere/miniprogram/pages/absence/absence.js
@@ -1,16 +1,60 @@
 Page({
   data: {
     listData: [
-      { "code": "01", "text": "高等数学", "type": "0" },
-      { "code": "02", "text": "C语言程序设计", "type": "0" },
-      { "code": "03", "text": "线性代数", "type": "0" },
-      { "code": "04", "text": "离散数学", "type": "0" },
-      { "code": "05", "text": "大学英语", "type": "0" },
-      { "code": "06", "text": "大学体育", "type": "1" },
-      { "code": "07", "text": "大学物理", "type": "0" }
+      { "code": "01", "text": "高等数学" },
+      { "code": "02", "text": "C语言程序设计"},
+      { "code": "03", "text": "线性代数" },
+      { "code": "04", "text": "离散数学"},
+      { "code": "05", "text": "大学英语"},
+      { "code": "06", "text": "大学体育"},
+      { "code": "07", "text": "大学物理"}
     ]
   },
   onLoad: function () {
     console.log('onLoad')
-  }
+  },
+  download: function (e) {
+    console.log(e);
+    let type = e.currentTarget.dataset.type;
+    let url = e.currentTarget.dataset.url;
+    switch (type) {
+      case "pdf":
+        url += 'pdf';
+        break;
+      case "word":
+        url += 'docx';
+        break;
+      case "excel":
+        url += 'xlsx';
+        break;
+      default:
+        url += 'pptx';
+        break;
+    }
+    wx.downloadFile({
+      url: url,
+      header: {},
+      success: function (res) {
+        var filePath = res.tempFilePath;
+        console.log(filePath);
+        wx.openDocument({
+          filePath: filePath,
+          success: function (res) {
+            console.log('打开文档成功')
+          },
+          fail: function (res) {
+            console.log(res);
+          },
+          complete: function (res) {
+            console.log(res);
+          }
+        })
+      },
+      fail: function (res) {
+        console.log('文件下载失败');
+      },
+      complete: function (res) { },
+    })
+
+  },
 })
