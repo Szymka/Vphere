d96159a723d34f0f66640ad343ae7aa0c4b0c4fa
Szymka <1210022074@qq.com>
2020-05-26 13:25:03 +0800

    前端新增订阅消息授权

--- frontend/Vphere/miniprogram/pages/index/index.js
+++ frontend/Vphere/miniprogram/pages/index/index.js
@@ -1,9 +1,31 @@
-const app = getApp()
+const app = getApp();
+const templateId = 'MQJIS_Fcpz7-nCyJ5QjRpoaP0F9cw5Yvq2rPCM4Ad9c';
 Page({
   data: {
 
   },
-
+test:function(e){
+  wx.requestSubscribeMessage({
+    tmplIds: [templateId],
+    success(res) {
+      if (res[templateId] == 'accept') {
+        wx.showToast({
+          title: '订阅成功'
+        })
+      } else {
+        wx.showToast({
+          title: '订阅失败'
+        })
+      }
+    },
+    fail(res) {
+      console.log(res)
+    },
+    complete(res) {
+      console.log(res)
+    }
+  })
+},
   onLoad: function () {
     var that = this
     wx.getUserInfo({
